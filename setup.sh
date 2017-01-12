882fc6e4f5219a686b7a43c7ae0b757c70a95b86
scriptype <scriptyper@gmail.com>
2017-01-12 16:24:49 +0300

    Add instruction for installing molokai colorscheme & create prerequisited folders

--- setup.sh
+++ setup.sh
@@ -1,18 +1,25 @@
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
+mkdir -p ~/.vim/colors && \
+curl https://raw.githubusercontent.com/crusoexia/vim-monokai/master/colors/monokai.vim > ~/.vim/colors/monokai.vim
+
+mkdir ~/.vim/backup
+mkdir ~/.vim/swp
+mkdir ~/.vim/undo
+
 git clone https://github.com/scriptype/vimrc ~/vimrc
 ln -s ~/vimrc/.vimrc ~/.vimrc
 
-cd ~/.vim/bundle/ && git clone git@github.com:Raimondi/delimitMate.git
-cd ~/.vim/bundle/ && git clone git://github.com/airblade/vim-gitgutter.git
+cd ~/.vim/bundle/ && git clone https://github.com/Raimondi/delimitMate.git
+cd ~/.vim/bundle/ && git clone https://github.com/airblade/vim-gitgutter.git
 cd ~/.vim/bundle/ && git clone https://github.com/scrooloose/nerdtree.git
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 git clone http://github.com/sjl/gundo.vim.git ~/.vim/bundle/gundo
-cd ~/.vim/bundle/ && git clone git@github.com:kien/ctrlp.vim.git
+cd ~/.vim/bundle/ && git clone https://github.com/kien/ctrlp.vim.git
 cd ~/.vim/bundle && git clone https://github.com/scrooloose/nerdcommenter.git
-cd ~/.vim/bundle && git clone git@github.com:ervandew/supertab.git
-cd ~/.vim/bundle && git clone git://github.com/tpope/vim-fugitive.git
+cd ~/.vim/bundle && git clone https://github.com/ervandew/supertab.git
+cd ~/.vim/bundle && git clone https://github.com/tpope/vim-fugitive.git
 vim -u NONE -c "helptags vim-fugitive/doc" -c q
-git clone git@github.com:powerline/fonts.git ~/power-fonts
+git clone https://github.com/powerline/fonts.git ~/power-fonts
 cd ~/power-fonts && sh install.sh
